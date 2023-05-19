3339acaaadba992e3bdee4c080c38ce22d389b24
Eric-Martin <gaspar.ericmartin@gmail.com>
2023-05-19 12:23:34 -0400

    Complete intro to js 2 looping code along

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+function writeCards(names, message) {
+    const messages = [];
+    for (let counter = 0; counter < names.length; counter++) {
+        messages.push(`Thank you, ${names[counter]}, for the wonderful ${message} gift!`);
+    }
+    return messages;
+}
+
+function countDown(number) {
+    /*for (let num = number; num >= 0; num--) {
+        console.log(num);
+    }*/
+    let num = number;
+    while(num >= 0) {
+        console.log(num)
+        num--
+    }
+}
+
